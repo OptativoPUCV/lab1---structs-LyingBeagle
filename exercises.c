3dd1ac8f1c18734513e665a91f1ab837ad5d95c4
Replit user <>
2023-08-14 03:19:58 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -49,11 +49,8 @@ int *filterEvenNumbers(int arr[], int size, int *newSize) {
   int *arrPares = (int *)malloc(sizeof(int));
   
   if (arrPares == NULL) {
-    *newSize = 0;
     return NULL;
   }
-  
-  *newSize = 0;
   
   for (int i = 0; i < size; i++) {
     if (arr[i] % 2 == 0) {
